a11d62f50d0dd0d2f78949d21e50a1d93985d919
Shreya.23 <shreyadutta760@gmail.com>
2026-06-14 12:41:40 +0530

    done

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,10 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './utils/db.js';
 import userRoutes from './routes/user.route.js';
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+
 
 dotenv.config({});
 
@@ -14,15 +18,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'http://localhost:5173', // Replace with your frontend URL
-    credentials: true, // Allow cookies to be sent
+    origin: 'http://localhost:5173', 
+    credentials: true, 
 }
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
 //api's
-app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
 app.listen(PORT,()=>{
     connectDB();
